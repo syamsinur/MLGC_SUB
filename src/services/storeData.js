cf9d4bb240fca22ff5db1a3b357835c518d4c99f
M. Nur Syamsi Maulidi <syamsinur455@gmail.com>
2024-05-23 22:25:07 +0700

    ganti db

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,7 +1,9 @@
 const { Firestore } = require('@google-cloud/firestore');
 
 async function storeData(id, data) {
-  const db = new Firestore();
+  const db = new Firestore({
+    databaseId: 'db-dinda'
+  });
 
   const predictCollection = db.collection('predictions');
   return predictCollection.doc(id).set(data);
